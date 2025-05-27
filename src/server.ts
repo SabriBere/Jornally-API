ffc7e58c46ba5c08b2d0d91481fb09f9c985859f
Sabrina Demetrio <sabrinab786@gmail.com>
2025-05-27 15:32:37 -0300

    configuración de eslint + prettier

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,22 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 //llamado a las rutas
 //llamado a swagger para documetación de end points
 //middleware para cuando no encuentra una ruta
 //configurar morgan para más info
 
-
-const server = express()
-server.use(express.json())
-server.use(cors()) //modo wildcard
+const server = express();
+server.use(express.json());
+server.use(cors()); //modo wildcard
 
 // server.use("/src", routes)
 
-async function startServer(){
+async function startServer() {
     server.listen(process.env.PORT, () => {
-        console.log("Server running on port:", process.env.PORT)
-        console.log("API version:", process.env.npm_package_version)
+        console.log("Server running on port:", process.env.PORT);
+        console.log("API version:", process.env.npm_package_version);
         console.log("Entorno:", process.env.NODE_ENV);
-    })
+    });
 }
 
-startServer()
+startServer();
